330923af6256b1a7fa22072ea33d1cb7170d2c06
heeseon0201 <rhehfdl9586@naver.com>
2025-06-13 16:33:50 +0900

    작업사항: . 깃 커밋 테스트

--- src/main/kotlin/com/altkey/code/store/order/spring/StoreOrderProperties.kt
+++ src/main/kotlin/com/altkey/code/store/order/spring/StoreOrderProperties.kt
@@ -1,12 +1,11 @@
 package com.altkey.code.store.order.spring
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 
 @Component
 class StoreOrderProperties (
-    private val environment: Environment
+    val environment: Environment
 
 ) {
     val applicationName: String = environment.getProperty("spring.application.name", "")
