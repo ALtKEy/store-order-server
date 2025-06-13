9992c2e4faa697673f436a98404507e5d56e0d77
heeseon0201 <rhehfdl9586@naver.com>
2025-06-13 17:30:49 +0900

    작업사항: . duckdb 버전 처리

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-	implementation("org.duckdb:duckdb_jdbc:1.3.0")
+	implementation("org.duckdb:duckdb_jdbc:1.3.0.0")
 	implementation("com.zaxxer:HikariCP:6.3.0")
 
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
